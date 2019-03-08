2dd5494a76135e78d87f55628782ab3f156a134a
ihsantegarpangestu <48311012+ihsantegarpangestu@users.noreply.github.com>
2019-03-08 09:03:42 +0700

    Add files via upload

---
+++ Ihsan_Tegar_Pangestu_17090089.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GL62M-7REX
+ */
+public class Ihsan_Tegar_Pangestu_17090089 {
+        public static void main(String[] args) {
+        System.out.println("Nama    :Ihsan Tegar Pangestu");
+        System.out.println("Nim     :17090089");
+        System.out.println("Kelas   :4A");
+        }
+}
